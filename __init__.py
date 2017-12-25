547b23c1a6cd09036707a0072c4c0b5d676b0132
puzzledsean <puzzledsean@gmail.com>
2017-12-25 14:07:40 -0500

    [ep7] tabs with hardcoded content

--- __init__.py
+++ __init__.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def homepage():
     return render_template("index.html")
 
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run()
