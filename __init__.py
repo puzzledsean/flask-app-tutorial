d9e42cf4dcbb6818bcf14323cba57b7d0ea65c8f
puzzledsean <puzzledsean@gmail.com>
2017-12-26 14:19:18 -0500

    [ep12] add user login logic

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from cms import Content
 
 TOPIC_DICT = Content()
@@ -19,7 +19,26 @@ def page_not_found(e):
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login_page():
-    return render_template("login.html")
+    error = '' 
+    try:
+        if request.method == "POST":
+            print('recognized post')
+            attempted_username = request.form['username']
+            attempted_password = request.form['password']
+            print(attempted_username)
+            print(attempted_password)
+            
+            if attempted_username == 'admin' and attempted_password == 'password':
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid credentials. Try again'
+
+        return render_template('login.html', error=error)
+
+    except Exception as e:
+        print('Error logging in')
+        return render_template("login.html", error=error)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
